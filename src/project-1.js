03170d5df0ce727c3804ac5821aa40b221cf58e8
fatinaqida <spm6834@psu.edu>
2024-04-07 17:54:14 -0400

    update

--- src/project-1.js
+++ src/project-1.js
@@ -1,90 +1,87 @@
 import "@lrnwebcomponents/rpg-character/rpg-character.js";
 import { html, css } from 'lit';
 import { DDD } from "@lrnwebcomponents/d-d-d/d-d-d.js";
+import { RpgCharacter } from "@lrnwebcomponents/rpg-character/rpg-character.js";
 
 export class project1 extends DDD {
 
-    static get tag() {
-        return 'project-1';
-      }
-    
-      constructor() {
-        super();
-        //this.input = "";
-        this.inputcount = 5;
-        this.number = 5;
-        this.party = [];
-        this.userName = null;
-      }
+  static get tag() {
+    return 'project-1';
+  }
 
-    static get styles () {
-        return css`
+  constructor() {
+    super();
+    this.index = 0;
+    this.userName = null;
+    this.changed = false;
+    this.saved = false;
+    this.party =
+      localStorage.getItem("party") != null
+        ? localStorage.getItem("party").split(",")
+        : [];
+  }
+
+  static get styles() {
+    return css`
 
           /**use var(ddd) for the height, width */
           :host {
-            --basic-color: black;
-            --text-color: #cfa0f8;
-            display: flex;
+            --basic-color: var(--ddd-theme-default-roarLight);
+            --text-color: var(--ddd-theme-default-potentialMidnight);
+            --basic-spacing: var(--ddd-spacing-2);
+            --standard-spacing: var(--ddd-spacing-0);
+            display: inline-flex;
             color: var(--text-color);
             background: var(--basic-color);
-            margin: 0 auto;
-            font-size: 14px; 
-            font-family: Arial, sans-serif;
+            margin: var(--standard-spacing) auto;
+            font-size: var(--ddd-font-size-3xs);
+            font-family: "Press Start 2P", system-ui;
             background-clip: padding-box;
             -webkit-text-size-adjust: 100%;
             width: 100%;
             text-align: center;
-            height: var(--dd-spacing, 600px);
+          }
+
+          .header {
+            font-family: "Press Start 2P", system-ui;
           }
 
           .btnsave {
             display: block;
-            background-color: #e01c1c;
-            box-shadow: 0 5px #666;
+            background-color: var(--ddd-theme-default-athertonViolet);
+            box-shadow: 0 5px var(--ddd-theme-default-potential50);
             width: 100px;
             text-align: center;
             cursor: pointer; 
             border-radius: 10px;
-            box-shadow: 0 5px #6b6a6a;
             transition: background-color 0.3s ease;
+            margin-top: var(--basic-spacing);
             margin: auto;
           }
 
-        .btnsave:disabled {
-            background-color: #ccc;
-            cursor: not-allowed;
-        } 
-
-        .btnsave:hover {
-            display: inline-block;
-            background-color: #5BA0ED;
-        }
-
         #container {
-            text-align: center;
-            display: relative;
-            text-align: center;
-            justify-content: center;
-            width: 1200px;
-            height: var(--dd-spacing, 500px);
-            margin: 0 auto;
+          text-align: center;
+          justify-content: center;
+          width: 900px;
+          height: var(--dd-spacing, 500px);
+          margin: 50px;
+          border: 3px solid var(--ddd-theme-default-potentialMidnight);
         }
 
         .btnadd,
         .btnremove {
           display: inline-block;
-          background-color: #e01c1c;
-          box-shadow: 0 5px #666;
-          padding: 4px 4px;
+          background-color: var(--ddd-theme-default-athertonViolet);
+          box-shadow: 0 5px var(--ddd-theme-default-potential50);
+          padding: var(--dd-spacing-4);
           width: 100px;
           text-align: center;
           cursor: pointer; 
           border-radius: 10px;
-          box-shadow: 0 5px #6b6a6a;
           transition: background-color 0.3s ease;
-          margin: 0 auto;
-          margin-top: 10px;
-          margin-bottom: 20px;
+          margin: var(--standard-spacing) auto;
+          margin-top: var(--basic-spacing);
+          margin-bottom: var(--basic-spacing);
         }
 
         .userName {
@@ -99,83 +96,128 @@ export class project1 extends DDD {
         }
 
         .input-wrapper {
-          display: block;
           text-align: center;
-          margin: 0 auto;
+          margin: var(--standard-spacing) auto;
         }
 
         .rpg-selector {
-          display: inline-flex;
           margin: var(---dd-spacing-5);
           padding: var(---ddd-spacing-1);
           width: 250px;
+          display:block;
         }
 
         .rpg-character {
             opacity: 0.4;
+      
         }
 
-        .rpg {
-          display: inline-block;
+        rpg-character:hover {
+            border: 2px solid var(--ddd-theme-default-discoveryCoral);
+      
+          }
+        .rpg-party {
+          max-width: 90vw;
+          justify-content: center;
+          padding: var(--ddd-spacing-4);
+          display: inline-flex;
+          flex-direction: row;
+          flex-wrap: wrap;
         }
     `}
 
-    /**there's an easier way for the party */
-    render() {
-        return html` <confetti-container id="confetti">
-          <h1>Haxcms-Party-UI</h1>
+  /**there's an easier way for the party */
+  render() {
+    return html`
+      <audio id="coin-sound" src="media/coin sound.wav"></audio>
+      <audio id="remove-sound" src="media/remove sound.mp3"></audio>
+      <confetti-container id="confetti">
+          <h1 class="header">Haxcms-Party-UI</h1>
           <div id="container">
-            
             <div class="input-wrapper">
               <input type="text" id="inputbox" placeholder="search username">
               <button class="btnadd" @click="${this.addItem}" >Add</button>
-              <button class="btnremove" @click="${this.removeitem}" >Remove</button>
+              <button class="btnremove" @click="${this.removeUser}" >Remove</button>
             </div>
-            
-            <div class="rpg">
-              ${this.party.map(user => html` 
-              <div class="rpg-selector">
-                <rpg-character class="character" seed="${user}" walking></rpg-character>
+            <div class="rpg-party">
+              ${this.party.map((user, index) => html` 
+              <div class="rpg-selector" @click="${(e) => this.selectCharacter(e, index)}">
+                        <rpg-character class="character" seed="${user}" walking></rpg-character>
                 <p class="characterName">
                   ${user}
                 </p>
               </div>
               `)}
             </div>
-            <button class="btnsave" @click="${this.makeItRain}" ?disabled="${this.inputcount < this.number}">Save</button>
-
+            <button class="btnsave" @click="${this.saveItem}" >Save</button>
           </div>        
         </confetti-container>  
-    `;}
+    `;
+  }
 
-    /** add lowercase function*/
+  /** add lowercase function*/
 
-    addItem(){
-      const input = this.shadowRoot.getElementById('inputbox');
-      const username = input.value.trim();
+  saveItem() {
+    if (this.party.length >= 1) {
+      const myArray = this.party.toString();
+      localStorage.setItem("party", myArray);
+      this.saved = true;
+      this.shadowRoot.getElementById("remove-sound").play();
+      this.makeItRain();
+    }
+    else {
+      window.alert("You need at least 1 party member to save.");
+    }
+  }
 
-      if (username !== '' && this.party.length < this.number) {
-        if (/^[a-z0-9]{1,10}$/.test(username))
-        {
-          if(!this.party.includes(username))
-          {
-            this.party.push(username);
-            this.value = '';
-            this.requestUpdate();
-          }
+  selectCharacter(event, index) {
+    const selectedCharacter = event.target.closest('.rpg-selector');
+    const selectedUserName = selectedCharacter.querySelector('.characterName').innerText;
 
-          else {
-            window.alert("Username is already in the party.");
-          }
+    // Find index of the selected user in the party array
+    const selectedIndex = this.party.findIndex(user => user === selectedUserName);
+    this.index = selectedIndex;
+  }
+
+  removeUser() {
+    this.party.splice(this.index, 1);
+    this.party = [...this.party];
+    this.shadowRoot.getElementById("remove-sound").play();
+  }
+
+
+  displayItem(item) {
+    if (this.saved) {
+      return html`<rpg-character walking seed="${item}"></rpg-character>`;
+    } else {
+      return html`<rpg-character seed="${item}"></rpg-character>`;
+    }
+  }
+
+  addItem() {
+    const input = this.shadowRoot.getElementById('inputbox');
+    const username = input.value.trim();
+
+    if (username !== '') {
+      if (/^[a-z0-9]{1,10}$/.test(username)) {
+        if (!this.party.includes(username)) {
+          this.party.push(username);
+          this.value = '';
+          this.requestUpdate();
         }
-        
+
         else {
-          window.alert("Username must be lowercase and numbers only.");
+          window.alert("Username is already in the party.");
         }
       }
-    }
 
-    makeItRain() {
+      else {
+        window.alert("Username must be lowercase, numbers, and less than 10 characters only.");
+      }
+    }
+  }
+
+  makeItRain() {
     import("@lrnwebcomponents/multiple-choice/lib/confetti-container.js").then(
       (module) => {
         setTimeout(() => {
@@ -184,16 +226,21 @@ export class project1 extends DDD {
       }
     );
   }
-    
+
+  toggleChanged() {
+    this.changed = !this.changed;
+  }
+
   static get properties() {
-        return {
-          ...super.properties,
-        input: { type: String, reflect: true },
-        closed: { type: Boolean, reflect: true },
-        opened: { type: Boolean, reflect: true },
-        number: { type: String, reflect: true }, 
-        inputcount: { type: String, reflect: true },
-        party: { type: String, reflect: true }
+    return {
+      ...super.properties,
+      input: { type: String, reflect: true },
+      number: { type: String, reflect: true },
+      inputcount: { type: String, reflect: true },
+      party: { type: Array, reflect: true },
+      changed: { type: Boolean, reflect: true },
+      saved: { type: Boolean, reflect: true },
+      index: { type: Number, reflect: true }
     };
   }
 }
